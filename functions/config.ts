67a0ff8c99549df0d1b1592c114a93aefd3ec5e5
Yutthapichai <cancel_go@hotmail.com>
2019-08-01 21:44:16 +0700

    Bot auto reply with line and connect with dialogflow

---
+++ functions/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    lineMessagingApi: 'https://api.line.me/v2/bot/message',
+    lineHeaders: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer sA/fBBLwUMYz2UEUb0k1G7rTySTh32zoy1ypUCQ3q321rF0y1FmDBd6QZJZsjnrd8IwRRcDzy3b/YPBNOdNfMeqZdxGXtRVIg1f9JHy1wWFr3HbxEh43r82GVYr9hcWLKOlI7CUV7gnxbaDdG00j2wdB04t89/1O/w1cDnyilFU="
+    }
+  };
+  
---
+++ functions/src/index.ts
@@ -0,0 +1,66 @@
+import * as functions from 'firebase-functions';
+// import * as request from 'request';
+// import { config }  from '../config';
+
+
+//[1]เพิ่ม dialogflow-fulfillment library
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+const region = "asia-east2";
+const runtimeOptions: any = {
+  timeoutSeconds: 4,
+  memory: "2GB"
+};
+
+export const webhook = functions
+  .region(region)
+  .runWith(runtimeOptions)
+  .https.onRequest(async(req, res) => {
+    // console.log("LINE REQUEST BODY", JSON.stringify(req.body));
+    console.log("Dialogflow REQUEST BODY", JSON.stringify(req.body));
+
+    //[2] ประกาศ ตัวแปร agent
+    const agent = new WebhookClient({ request: req, response: res });
+
+
+    //[4] ทำ function view menu เพื่อแสดงผลบางอย่างกลับไปที่หน้าจอของ bot
+    const viewMenu = async (agents: any) => {
+      agent.add("นี่คือ Menu ที่มาจาก Fullfilment");
+    };
+
+
+    //[3] ทำ intent map เข้ากับ function
+    const intentMap = new Map();
+    intentMap.set("View-menu", viewMenu);
+    agent.handleRequest(intentMap);
+
+
+    //[0] ดึงข้อมูลจาก request message ที่มาจาก LINE
+    // const replyToken = req.body.events[0].replyToken;
+    // const messages = [
+    //   {
+    //     type: "text",
+    //     text: req.body.events[0].message.text //ดึง message ที่ส่งเข้ามา
+    //   },
+    //   {
+    //     type: "text",
+    //     text: JSON.stringify(req.body) //ลองให้พ่น สิ่งที่่ LINE ส่งมาให้ทั้งหมดออกมาดู
+    //   }
+    // ];
+
+    // //ยิงข้อความกลับไปหา LINE (ส่ง response กลับไปหา LINE)
+    // return await lineReply(replyToken, messages);
+  });
+
+  // const lineReply = async (replyToken: string, messages: any) => {
+  //   const body = {
+  //     replyToken: replyToken,
+  //     messages: messages
+  //   };
+  //   return await request({
+  //     method: "POST",
+  //     uri: `${config.lineMessagingApi}/reply`,
+  //     headers: config.lineHeaders,
+  //     body: JSON.stringify(body)
+  //   });
+  // };
